a58a90a16b14beeb8d84d9976a102292bcb0cccd
Artem <77139108+Artem45464@users.noreply.github.com>
2024-12-13 12:43:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
+// Function to change text when the button is clicked 
 function changeText() {
-    document.getElementById("about").innerHTML = "you clicked the button";
+    document.getElementById("about").innerHTML = "You clicked the button!";
 }
 
-// Script for habing the navbar active state 
+// Script for having the navbar active state 
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll(".nav-link");
 
     navLinks.forEach(link => {
         link.addEventListener("click", function (event) {
-            event.preventDefault(); // stops the defult navigtion behavior
+            event.preventDefault(); // stops the default navigation behavior
             navLinks.forEach(link => link.classList.remove("active")); // clear other active link 
             this.classList.add("active"); // set the clicked link as active
         });
     });
 });
-
-// Function to change text when the button is clicked 
-function changeText() {
-    document.getElementById("about").innerHTML = "You clicked the button!";
-}
